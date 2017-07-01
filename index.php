36c073a238dad4235d6f8346cefb62b4a39a5c4c
Ardi <ardiasllani@hotmail.com>
2017-07-01 00:14:02 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+error_reporting(0);
+include "gp.php";
+
+if($_POST['submit'] != ""){
+	$url = $_POST['url'];
+	$getGP = getPhotoGoogle($link);
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+	<title>Get link Google Drive</title>
+</head>
+<body>
+
+  <!-- Docs styles -->
+  <link rel="stylesheet" href="https://cdn.plyr.io/2.0.13/demo.css">
+	<style>
+		.container {
+		  max-width: 800px;
+		  margin: 0 auto;
+		}
+	</style>
+
+	<div class="container">
+		<br />
+		<form action="" method="POST">
+			<input type="text" size="80" name="url" value="https://photos.google.com/share/AF1QipP4SgmZ9Q2FWUKtE4rCwq1bwj-c_yBTPl7K5vZlQfTe5H9IlU4VGiCodMcJ6lDTlA/photo/AF1QipO_SW6Zov_mA-DORS6_eU4C3JdNlRFQTkQ6wlR8?key=Tm51bjVTNGN5c2RLQm1fcmpLWnIwbzRPVTZlTDhn"/>
+			<input type="submit" value="GET" name="submit" />
+		</form>
+		<br/>
+
+		<div id="myElement">Paste the url and click the get button.</div>
+
+	</div>
+
+	<script src="https://content.jwplatform.com/libraries/DbXZPMBQ.js"></script>
+	<script type="text/javascript">
+		jwplayer("myElement").setup({
+			playlist: [{
+				"sources":<?php echo $getGP?>
+			}],
+			allowfullscreen: true,
+			width: '100%',
+			aspectratio: '16:9',
+		});
+	</script>
+
+</body>
+</html>
