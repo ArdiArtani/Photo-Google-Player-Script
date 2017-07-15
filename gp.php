adaf23022107778cb156586cbcc508abcee31923
Ardi <ardiasllani@hotmail.com>
2017-07-15 13:43:28 -0400

    Fixed the quality order from high to low

--- gp.php
+++ gp.php
@@ -28,22 +28,24 @@ function getPhotoGoogle($link){
 	$decode = urldecode($url[0]);
 	$count = count($data);
 	$linkDownload = array();
-	if($count > 4) {
-		$v1080p = $decode.'=m37';
-		$v720p = $decode.'=m22';
+	if($count > 2) {
 		$v360p = $decode.'=m18';
-		$linkDownload['1080p'] = $v1080p;
-		$linkDownload['720p'] = $v720p;
 		$linkDownload['360p'] = $v360p;
 	}
+
 	if($count > 3) {
 		$v720p = $decode.'=m22';
 		$v360p = $decode.'=m18';
 		$linkDownload['720p'] = $v720p;
 		$linkDownload['360p'] = $v360p;
 	}
-	if($count > 2) {
+
+	if($count > 4) {
+		$v1080p = $decode.'=m37';
+		$v720p = $decode.'=m22';
 		$v360p = $decode.'=m18';
+		$linkDownload['1080p'] = $v1080p;
+		$linkDownload['720p'] = $v720p;
 		$linkDownload['360p'] = $v360p;
 	}
 
